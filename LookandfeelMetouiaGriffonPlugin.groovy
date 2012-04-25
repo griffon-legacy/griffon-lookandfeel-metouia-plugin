600cc5a96a57054fbdf0fe0a11436284829bd6ed
aalmiray <aalmiray@gmail.com>
2012-04-25 11:34:29 +0200

    Updates for Griffon 0.9.5

--- LookandfeelMetouiaGriffonPlugin.groovy
+++ LookandfeelMetouiaGriffonPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2010-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -18,20 +18,50 @@
  * @author Andres Almiray
  */
 class LookandfeelMetouiaGriffonPlugin {
-    def version = 0.3
-    def griffonVersion = '0.9.3 > *'
-    def dependsOn = [lookandfeel: 0.5]
-    def toolkits = ['swing']
-    def license = 'Apache Software License 2.0'
+    // the plugin version
+    String version = '0.4'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [lookandfeel: '0.6']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, gtk
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-lookandfeel-metouia-plugin'
 
-    def author = 'Andres Almiray'
-    def authorEmail = 'aalmiray@users.sourceforge.net'
-    def title = 'Metouia Swing Look & Feel'
-    def description = '''
-Metouia Swing Look & Feel.
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Pluggable Look & Feel based on Lipstik'
+    String description = '''
+Pluggable Look & Feel based on Metouia.
+
+Usage
+-----
+
+Refer to the [lookandfeel][1] plugin to find out how setup `lookAndFeel` and `theme` properties in the configuration.
+
+LookAndFeel and Themes
+----------------------
+| *LookAndFeel* | *Theme* |
+| ------------- | ------- |
+| Metouia       | Metouia |
+
+[1]: /plugin/lookandfeel
 '''
-
-    // URL to the plugin's documentation
-    def documentation = 'http://griffon.codehaus.org/Lookandfeel+Metouia+Plugin'
 }
 
--- griffon-app/conf/BuildConfig.groovy
+++ griffon-app/conf/BuildConfig.groovy
@@ -2,10 +2,9 @@ griffon.project.dependency.resolution = {
     inherits("global")
     log "warn"
     repositories {
-        griffonPlugins()
         griffonHome()
-        griffonCentral()
-        flatDir name: 'lookandfeelMetouiaPluginLib', dirs: 'lib'
+        String basePath = pluginDirPath? "${pluginDirPath}/" : ''
+        flatDir name: "lookAndFeelMetouiaLibDir", dirs: ["${basePath}lib"]
     }
     dependencies {
         runtime ':metouia:1.0.0-beta'
@@ -20,4 +19,16 @@ griffon {
     }
 }
 
-griffon.jars.destDir='target/addon'
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    appenders {
+        console name: 'stdout', layout: pattern(conversionPattern: '%d [%t] %-5p %c - %m%n')
+    }
+
+    error 'org.codehaus.griffon',
+          'org.springframework',
+          'org.apache.karaf',
+          'groovyx.net'
+    warn  'griffon'
+}
